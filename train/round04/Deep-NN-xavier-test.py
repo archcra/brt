2909258529c1a1f532e45353ec08b9b7e3b87519
Holibut <holibut@gmail.com>
2017-06-07 12:15:49 +0800

    Feat: Round4中，对于11类图形的测试及应用准确率，均达到100%

---
+++ train/round04/Deep-NN-xavier-test.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+import sys
+import loadApplyData
+import loadTrainData
+
+pixels = 70*74    
+imagesClass = 11
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+
+W1 = tf.get_variable("W1", shape=[pixels, pixels],
+     initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([pixels]))
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1) # L1 就是原来的layer1
+
+
+W2 = tf.get_variable("W2", shape=[pixels, 512],
+                    initializer=tf.contrib.layers.xavier_initializer())
+                    
+b2 = tf.Variable(tf.random_normal([512]), name='bias2')
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+
+
+W3 = tf.get_variable("W3", shape=[512, 512],
+                    initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([512]))
+L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+
+
+W4 = tf.get_variable("W4", shape=[512, 512],
+                    initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([512]))
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+
+
+W5 = tf.get_variable("W5", shape=[512, imagesClass],
+                    initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([imagesClass]))
+hypothesis = tf.matmul(L4, W5) + b5
+
+
+# Test model
+is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
+# Calculate accuracy
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+# Test the model using test sets
+
+imageName = 'data/Cr001.png'
+testImage = loadApplyData.getImageData(imageName)
+xData = [testImage]
+testLabel = loadTrainData.getLabelData('c')
+yData = [testLabel]
+modelName = "saved/model-deep-nn-xavier.ckpt"
+
+# Add ops to save and restore all the variables.
+saver = tf.train.Saver()
+
+
+# Launch graph
+with tf.Session() as sess:
+   # Restore variables
+   saver.restore(sess, modelName)
+   print("Accuracy: ", accuracy.eval(session=sess, 
+        feed_dict={X: xData, Y: yData}))
+
+
+
+
+
+
+
+
---
+++ train/round04/loadTrainData.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from struct import *
+import numpy, sys
+
+# http://stackoverflow.com/questions/25019287/how-to-convert-grayscale-values-in-matrix-to-an-image-in-python
+
+# end if
+imagesFile = 'data/brt-train-images03.data';
+labelsFile = 'data/brt-train-labels03.data';
+
+w, h = 70, 74 
+
+
+# 前10张图表示的数字为：05 00 04 01 09 02 01 03 01 04
+def getImagesData(): 
+    with open(imagesFile, mode='rb') as file: # b is important -> binary
+        fileContent = file.read()
+        imagesCount = unpack(">i", fileContent[4:4+4])
+        print (imagesCount[0])
+        xMatrix = [[0 for x in range(70*74)] for y in range(imagesCount[0])] 
+
+        # 5180 = 70*74
+        for nthImage in range(1, imagesCount[0] + 1):
+            imageBytes = unpack("5180c", fileContent[16+w*h*(nthImage-1):16+w*h*nthImage])
+            
+            for i in range(70*74):
+                xMatrix[nthImage-1][i] = ord(imageBytes[i])
+            # end for
+        # end for
+        
+    return xMatrix
+
+
+chessmenType = {
+    'a': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'b': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'c': [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    'k': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    'n': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    'p': [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    'r': [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    'B': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    'K': [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    'A': [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    'P': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    }
+    
+def getLabelData(label):
+    return chessmenType[label]
+    
+def getLabelsData(): 
+
+    with open(labelsFile, mode='rb') as file: # b is important -> binary
+        fileContent = file.read()
+        
+        labelsCount = unpack(">i", fileContent[4:4+4])
+        xMatrix = [0 for x in range(labelsCount[0])]  
+        labelsBytes = unpack(str(labelsCount[0]) + "c", fileContent[8:8+labelsCount[0]])
+        print(labelsBytes)
+        for i in range(labelsCount[0]):
+            # print('|'+labelsBytes[i]+'|')
+            xMatrix[i] = chessmenType[str(chr(ord(labelsBytes[i])))]
+        # end for
+        
+    return xMatrix
+    
+    
+if __name__ == '__main__':
+    # data = getImagesData()
+    # print (data)    
+    labels = getLabelsData()
+    print (labels)
+    
+
